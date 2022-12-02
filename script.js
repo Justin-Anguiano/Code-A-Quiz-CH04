790b3009ad4d3164c496062f926eae7104fb88c4
Justin_Anguiano <justin.anguiano@cox.net>
2022-12-02 11:59:11 -0600

    updated timer and buttons

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
-alert("Hello Human! 😀")
-alert("This quiz has a few rules!")
-alert("This is a timed quiz.")
-alert("There is total of 4 questions")
-alert("Your score will be displayed upon completion 🙃")
+// alert("Hello Human! 😀");
+// alert("This quiz has a few rules!");
+// alert("You have 3 minutes to complete.");
+// alert("There is total of 4 questions");
+// alert("Your score will be displayed upon completion 🙃");
 
-var startMin = 3;
-let time = startMin * 60
+var second = 60;
+var minute = 2;
 
-var countBtnEl = document.getElementById("countdown");
-setInterval(updateTimer, 1000)
+setInterval(function () {
+  if (minute == 0 && second == 1) {
+    document.getElementById("counter").innerHTML = "03:00";
+  } else {
+    second--;
+    if (second == 0) {
+      minute--;
+      second == 60;
 
-function updateTimer()  {
-  var minutes = Math.floor(time/60);
-  let seconds = time % 60;
-  time--;
-}
+      if (minute == 0) {
+        minute = minute;
+      }
+    }
+    if (minute.toString().length == 1) {
+      minute = "0" + minute;
+    }
+    if (second.toString().length == 1) {
+      second = "0" + second;
+    }
+    document.getElementById("counter").innerHTML = minute + ":" + second;
+  }
+}, 1000);
 
 //function beginQuiz()
--- script2.js
+++ script2.js
@@ -40,24 +40,6 @@ var theQuestions = [
     },
     correctAnswer: "b"
 }]
-var questionIndex = -1; // Not started
 
-function nextQuestion() {
-document.body.innerHTML = '';
-  ++questionIndex;
-    document.write(theQuestions[questionIndex].ques + "<br />");
 
-        for (var i = 0; i < theQuestions[questionIndex].choices.length; i++) {
-        document.write("<input type=radio id=myRadio name=radAnswer>" + theQuestions[questionIndex].choices[i] + "<br />");
-        }
-        
-   if (questionIndex < (theQuestions.length - 1)) {
-    var nextButton = document.createElement("input");
-    nextButton.type = "button";
-    nextButton.value = "Next question";
-    nextButton.addEventListener('click', nextQuestion);
-    document.body.appendChild(nextButton);
-   }
-};
 
-nextQuestion();
