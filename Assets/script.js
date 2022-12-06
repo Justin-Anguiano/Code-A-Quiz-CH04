cf8ab8247f462c903387cb71fcc9aeeec6d25ebe
Justin_Anguiano <justin.anguiano@cox.net>
2022-12-05 19:17:02 -0600

    updated HTML, readme file

--- Assets/script.js
+++ Assets/script.js
@@ -1,23 +1,18 @@
-// alert("Hello Human! 😀 This quiz has a few rules!");
-// alert("You have 3 minutes (180 seconds) to complete.");
-// alert("There is total of 4 questions");
-// alert("Upon completion, click on the Results button for your score 🙃");
+alert("Hello Human! 😀 This quiz has a few rules!");
+alert("You have 3 minutes (180 seconds) to complete.");
+alert("There is total of 3 questions");
+alert("Upon completion, click on the Results button for your score 🙃");
 
 var startButton = document.getElementById("start-btn");
-var questionContainerElement = document.getElementById("question-container");
-var questionElement = document.getElementById("question");
-//var answerButtonsElement = document.getElementById("answer-buttons");
 
-let currentQuestionIndex;
+var questionElement = document.getElementById("questions1");
 
-startButton.addEventListener("click", startGame);
-
+startButton.addEventListener("click", startQuiz);
+//start quiz and timer together.
 function startQuiz() {
-  console.log("Started");
-  startButton.classList.add("hide");
-  currentQuestionIndex = 0;
-  //questionContainerElement.classList.remove("hide");
+  countdown();
 }
+
 
 var timerEl = document.getElementById("countdown");
 var mainEl = document.getElementById("main");
@@ -25,10 +20,10 @@ var mainEl = document.getElementById("main");
 function countdown() {
   var timeLeft = 180;
 
-  // TODO: Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  // `setInterval()` method to call the function
   var timeInterval = setInterval(function () {
     timeLeft--;
-    timerEl.textContent = timeLeft + " seconds remaining!";
+    timerEl.textContent = timeLeft + " seconds left!";
 
     if (timeLeft === 0) {
       clearInterval(timeInterval);
@@ -40,40 +35,15 @@ function countdown() {
 // Displays the message one word at a time
 function displayMessage() {
   var wordCount = 0;
-  // Uses the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  
   var msgInterval = setInterval(function () {
-    // If there are no more words left in the message
+    
     if (words[wordCount] === undefined) {
       // Use `clearInterval()` to stop the timer
       clearInterval(msgInterval);
     } else {
-      // Display one word of the message
       mainEl.textContent = words[wordCount];
       wordCount++;
     }
   }, 1000);
 }
-// countdown();
-// var correctAnswer = ""
-// var question = {[
-//      // {
-//       question: "What is the world's most popular programming language?",
-//       answers: ["Java", "JavaScript", "React"]
-//       correctAnswer: 1]};
-// ,
-//       {
-//       question: "What are the 4 ways to declare a variable?",
-//       answers: ["var", "let", "const", "all of these"]
-//       correctAnswer: 3},
-
-//       {
-//       question: "What characters can be in a function name?",
-//       answers: ["letters", "digits", "underscores", "dollar signs $", "all of these"]
-//       correctAnswer: 4},
-
-//       {
-//       question: "Of the following, which is a correct conditional statement?",
-//       answers: ["then", "if", "next", "elves"]
-//       correctAnswer: 1}
-//     ]};
-// function beginQuestions(document.getElementById("question"));
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Code-A-Quiz-CH04
+
+Description: This is incomplete but, the thought was to develop a multiple choice quiz.
+Each question was to populate independently and time was to be reduced by 10 seconds per every wrong answer. It was also intended record high scores.
+
+Installation: N/A
+
+Usage: Instructions land on the page click ok through the alerts and then press start to begin. Once start is pressed the timer begins the countdown. Players are to answer each question and quickly and precisely as possible with the ability to record their score.
+
+Topics: HTML,CSS, Javascript learned
+
+Credits: N/A
+
+License: N/A
+
+Features: N/A
+
+
